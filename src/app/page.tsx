ddb0c638da0f4d0abc6f67146db1480298ec725d
ilya123558ilya <ilya123558ilya@gmail.com>
2025-04-10 10:18:14 +0300

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import { Loader } from "@/shared/ui/loader/Loader";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import { retrieveLaunchParams } from '@telegram-apps/bridge';
 import { useLoginByInitDataMutation } from "@/entities/users/api/users.api";
 import { setUser, useAppDispatch } from "@/views/store";
 
@@ -11,14 +12,34 @@ export default function Page() {
   const [ loginByInitData, {data: user, isSuccess} ] = useLoginByInitDataMutation()
 
   useEffect(() => {
-    if (typeof window !== "undefined" && window.Telegram && window.Telegram.WebApp) {
-      const data_init = window.Telegram.WebApp.initData;
+    try {
+      const launchParams = retrieveLaunchParams();
   
-      if (data_init) {
-        loginByInitData({ data_init });
+      // Проверка на наличие параметров и данных пользователя
+      if (launchParams?.tgWebAppData?.user) {
+        const data_init = JSON.stringify(launchParams.tgWebAppData.user);
+  
+        // Если данные присутствуют, выполняем запрос
+        if (data_init) {
+          loginByInitData({ data_init })
+            .unwrap()  // Если используешь RTK Query с unwrap для получения данных напрямую
+            .then((response) => {
+              console.log("User data successfully sent:", response);
+            })
+            .catch((error) => {
+              console.error("Error during login:", error);
+            });
+        } else {
+          console.error("User data not found in launchParams.");
+        }
+      } else {
+        console.error("Telegram WebApp data or user not found in launchParams.");
       }
+    } catch (error) {
+      console.error("Error retrieving launch parameters:", error);
     }
   }, []);
+  
 
   useEffect(() => {
     if(user && isSuccess) {
