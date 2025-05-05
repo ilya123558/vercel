60e9af47a590a100acb74a714cff0bf7edd2a002
ilya123558ilya <ilya123558ilya@gmail.com>
2025-05-06 00:49:20 +0300

    20:08

--- src/features/coin-content/CoinContent.tsx
+++ src/features/coin-content/CoinContent.tsx
@@ -5,12 +5,11 @@ import { motion } from "framer-motion";
 import animationData from '@/views/data/coin-animation.json'
 
 export const CoinContent = () => {
-
   return (
     <motion.div {...animationImg} className="flex h-full items-center justify-center relative">
       <div className="absolute bg-[#5D3EC1] rounded-full blur-[60px] max-w-160px max-h-160px w-160px-h h-160px-h"></div>
       {/* <Image style={{zIndex: 10}} onClick={handleStartGame} src={'/images/home/coin.png'} alt="coin" width={270} height={270} quality={100} className="max-w-250px max-h-250px w-250px-h h-250px-h" /> */}
-      <CoinAnimation animationData={animationData} />
+      {/* <CoinAnimation animationData={animationData} /> */}
     </motion.div>
   );
 };
