b8adb9b163265cd7dda38459e247739289dd45bf
ilya123558ilya <ilya123558ilya@gmail.com>
2025-05-06 01:07:05 +0300

    20:07

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,18 +3,9 @@ import { GameInfo } from "@/features/game-info/GameInfo";
 import { Container } from "@/shared/ui/container/Container";
 import { Checklist } from "@/widgets/checklist/Checklist";
 import { Game } from "@/widgets/game/Game";
-import { Header } from "@/widgets/header/Header";import { useEffect, useState } from "react";
-;
+import { Header } from "@/widgets/header/Header";
 
 export default function Page() {
-  const [isClient, setIsClient] = useState(false);
-
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
-
-  if (!isClient) return <></>; 
-
   return ( 
     <section className="">
       <Container>
