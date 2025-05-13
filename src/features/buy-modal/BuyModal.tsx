1a7c4d6961dab408c33260279959965b40370330
ilya123558ilya <ilya123558ilya@gmail.com>
2025-05-13 21:34:09 +0300

    fix

--- src/features/buy-modal/BuyModal.tsx
+++ src/features/buy-modal/BuyModal.tsx
@@ -30,7 +30,7 @@ export const BuyModal = ({ isOpen, setIsOpen, title, id, price, isActive, photo,
   useEffect(() => {
     if(buySuccess && activateData) {
       setIsOpen(false)
-      dispatch(setUserBackground(activateData.customization.source))
+      dispatch(setUserBackground(activateData.customization.photo))
       handleNotification("buy success")
     }
     if(activateSuccess) {
